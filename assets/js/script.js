2dd1943cf1b23e98180253e9d049775f932218d9
carlos caceres <carlos.caceres.mt@usa.edu.co>
2022-10-11 08:51:28 -0500

    frontend vista tabla

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const endpoint = "http://localhost:8080/api/Client"
+
+$(document).ready(function() {
+    getClient()
+})
+
+function getClient() {
+    let tam = 0;
+    $.ajax({
+        url: endpoint + "/all",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            let tam = data.length
+            if (tam == 0) {
+                $("#contenedor").hide()
+                $("#mensaje").show()
+            } else {
+                $("#contenedor").show()
+                $("#mensaje").hide()
+                $("#numregistro").html("Numero de Registros: " + tam)
+                console.log(data)
+                let registro = ""
+                $.each(data, function(index, client) {
+                    registro += "<tr>" +
+                        "<td>" + client.idClient + "</td>" +
+                        "<td>" + client.email + "</td>" +
+                        "<td>" + client.password + "</td>" +
+                        "<td>" + client.name + "</td>" +
+                        "<td>" + client.age + "</td>" +
+                        "</tr>"
+                })
+                console.log(registro)
+                $("#tbody").html(registro)
+            }
+        }
+    })
+
+}
